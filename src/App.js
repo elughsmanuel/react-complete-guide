3c6ed6289a575dc249b9226a9ce38dff3c2c604a
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-08-31 11:55:27 +0100

    Adding form input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 
 const App = () => {
   const expenses = [
@@ -31,7 +33,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h2>Let's get started</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
