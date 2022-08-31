26bd110ffddc534ee9ea86bea17788da78d04f57
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-08-31 13:33:31 +0100

    Working with events completed

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
+import { useState } from 'react';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
   return (
     <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
         <ExpenseItem
             title={props.items[0].title}
             amount={props.items[0].amount}
